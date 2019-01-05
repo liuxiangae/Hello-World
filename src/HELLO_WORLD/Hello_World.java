fc947c5079caaf48dbc3392dbaad478ea94950b4
1160300325 <15117884595@139.com>
2019-01-05 21:12:20 +0800

    first commit“

---
+++ src/HELLO_WORLD/Hello_World.java
@@ -0,0 +1,11 @@
+package HELLO_WORLD;
+
+public class Hello_World {
+   /*this is my first github project .and i want to give it to my girlfriend :°¡¶ð
+    * author:LX
+    * time:2019:1:5
+    */
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+	}
+}
